20f53bc31114d9fcfc6df3791c3a8c0a97712459
Juan De Hoyos <juancamilodehoyos8@gmail.com>
2024-06-24 08:26:48 -0500

    adding more configuration for the cors

--- dentalclinic/settings.py
+++ dentalclinic/settings.py
@@ -108,6 +108,31 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'dentalclinic.urls'
 
 TEMPLATES = [
