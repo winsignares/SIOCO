73cbdaac07ec3e419c33b1184aaac8ff364bd782
Juan De Hoyos <juan.hoyos@grupo1522.com>
2024-09-01 21:22:53 -0500

    modificando inicio de sesión para retornar con más de un dominio, eliminando endpoint decode token

--- shared/views/auth_views.py
+++ shared/views/auth_views.py
@@ -3,14 +3,15 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from ..serializers import AuthTokenSerializer
+from rest_framework.permissions import AllowAny
+from ..serializers import AuthTokenSerializer, OdontologyDomainSerializer
 from ..models import OdontologyDomain, OdontologyUser
 
 class Login(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
+
         serializer = AuthTokenSerializer(data=request.data)
         if serializer.is_valid():
             username = serializer.validated_data['username']
@@ -29,7 +30,7 @@ class Login(APIView):
                         odontology = {
                             'id': clinic.odontology.pk,
                             'name': clinic.odontology.name,
-                            'domain_url': OdontologyDomain.objects.get(tenant_id=clinic.odontology.pk).domain
+                            'domains_url':  OdontologyDomainSerializer(OdontologyDomain.objects.filter(tenant_id=clinic.odontology.pk), many=True).data
                         }
                         odontologies.append(odontology)
                     
@@ -50,26 +51,3 @@ class Login(APIView):
                 return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-class DecodeToken(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        auth_header = request.headers.get('Authorization')
-        if not auth_header:
-            return None
-        try:
-            token_key = auth_header.split(' ')[1]
-            token = Token.objects.get(key=token_key)
-            user_id = token.user.pk
-            print(token.user.role)
-            print(f'Debug: Token obtenido para user_id={user_id}')
-            
-            return Response({
-                'user_id': user_id,
-                'created': token.created,
-                'token_key': token.key
-            }, status=status.HTTP_200_OK)
-        except Token.DoesNotExist:
-            return None
-    
