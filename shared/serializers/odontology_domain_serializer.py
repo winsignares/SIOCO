987d7dbba884c76bec7f5800c7d8b72ad631fabd
Juan De Hoyos <juancamilodehoyos8@gmail.com>
2024-09-08 18:46:05 -0500

    eliminando serializer duplicado y eliminando endpoint decode

--- shared/serializers/odontology_domain_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import OdontologyDomain
-
-
-class OdontologyDomainSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OdontologyDomain
-        fields = ['domain']
--- shared/views/auth_views.py
+++ shared/views/auth_views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from ..serializers import AuthTokenSerializer
 from ..models import OdontologyDomain, OdontologyUser
+
 
 class Login(APIView):
     permission_classes = [AllowAny]
@@ -19,11 +20,11 @@ class Login(APIView):
             username = serializer.validated_data['username']
             password = serializer.validated_data['password']
             user = authenticate(username=username, password=password)
-            
+
             if user is not None:
                 token, created = Token.objects.get_or_create(user=user)
                 odontologies = []
-                
+
                 print(f'user role: {user.role}')
                 if str(user.role) == "patient":
 
@@ -32,47 +33,25 @@ class Login(APIView):
                         odontology = {
                             'id': clinic.odontology.pk,
                             'name': clinic.odontology.name,
-                            'domain_url': OdontologyDomainSerializer(OdontologyDomain.objects.filter(tenant_id=clinic.odontology.pk), many=True).data
+                            'domain_url': OdontologyDomainSerializer(
+                                OdontologyDomain.objects.filter(tenant_id=clinic.odontology.pk), many=True).data
                         }
                         odontologies.append(odontology)
-                    
+
                     token_data = {
                         'token': token.key,
                         'odontologies': odontologies,
                         'role': user.role.name
                     }
-                    
+
                 else:
                     token_data = {
                         'token': token.key,
                         'role': user.role.name
                     }
-                    
+
                 return Response(token_data)
             else:
                 return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-class DecodeToken(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        auth_header = request.headers.get('Authorization')
-        if not auth_header:
-            return None
-        try:
-            token_key = auth_header.split(' ')[1]
-            token = Token.objects.get(key=token_key)
-            user_id = token.user.pk
-            print(token.user.role)
-            print(f'Debug: Token obtenido para user_id={user_id}')
-            
-            return Response({
-                'user_id': user_id,
-                'created': token.created,
-                'token_key': token.key
-            }, status=status.HTTP_200_OK)
-        except Token.DoesNotExist:
-            return None
-    
